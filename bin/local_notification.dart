8fb21e86980d37bc04051ea2244fea0d31073cfb
Marawan <Marawankheer94@gmail.com>
2023-01-01 12:21:03 +0200

    1st

--- bin/local_notification.dart
+++
@@ -1,5 +0,0 @@
-import 'package:local_notification/local_notification.dart' as local_notification;
-
-void main(List<String> arguments) {
-  print('Hello world: ${local_notification.calculate()}!');
-}
